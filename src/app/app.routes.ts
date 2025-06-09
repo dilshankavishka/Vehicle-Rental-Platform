e5ab9abd7358611e62727f5b49d3d50ef8aee667
dilshankavishka <dilshankavishka888@gmail.com>
2025-06-09 14:41:46 +0530

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { SocialsComponent } from './socials/socials.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'socials', component: SocialsComponent },
+];
